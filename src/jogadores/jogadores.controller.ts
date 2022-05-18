b71a0165e3532689b0073787b5d117eb77fd3b92
Gustavo Fontana <gustavoasf77@gmail.com>
2022-05-18 15:18:59 -0300

    Create service, using Injectable, add uuid

--- src/jogadores/jogadores.controller.ts
+++ src/jogadores/jogadores.controller.ts
@@ -1,14 +1,16 @@
+import { JogadoresService } from './jogadores.service';
 import { Body, Controller, Post } from '@nestjs/common';
 import { CriarJogadorDto } from './dtos/criar-jogador.dto';
 
 @Controller('api/v1/jogadores')
 export class JogadoresController {
+
+  constructor(private readonly jogadoresService: JogadoresService) {}
+
   @Post()
   async criarAtualizarJogador(
     @Body() criaJogadorDto: CriarJogadorDto) {
-    const { email } = criaJogadorDto;
-    return JSON.stringify(`{
-      "email": ${email},
-    }`);
-  }
+      await this.jogadoresService.criarAtualizarJogador(criaJogadorDto)
+    }
+  
 }
